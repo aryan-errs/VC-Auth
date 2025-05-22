9e4acb9548ad2c02f0caf495014fbba722e4829a
Aryan Sharma <aryan182282@gmail.com>
2025-05-22 15:52:50 +0530

    added readme

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,17 +2,11 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
 
-const HARMONY_PRIVATE_KEY =
-  "0d065b1a5dc3f5e336f20b51e7c0cca40bcffe17b64d9e01a0fa0a2188d1904a"; //Example Harmony Devnet privkey
 const SEPOLIA_PK =
   "566dc65ca94cc3e69bf6a0170be2f94752dace6c724b4050bdcdcddc3f8c948c";
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
-    harmonydevnet: {
-      url: `https://api.s0.ps.hmny.io`,
-      accounts: [`0x${HARMONY_PRIVATE_KEY}`],
-    },
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/Dp3-dMBc1XWm2veRNbRiTFw-dUddq1c7`,
       accounts: [`0x${SEPOLIA_PK}`],
